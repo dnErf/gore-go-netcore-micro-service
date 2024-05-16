779af25592bfbb983f31b4548ff71b109463d51e
ff <frend.fernando@LIVE.COM>
2024-05-16 13:58:13 -0500

    ft home page layout init

--- src/go_service/main.go
+++ src/go_service/main.go
@@ -12,6 +12,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", views.RenderHome).Methods("GET")
 
+	router.PathPrefix("/bin/").Handler(http.StripPrefix("/bin/", http.FileServer(http.Dir("bin"))))
+
 	fmt.Print("listening to :3000")
 	http.ListenAndServe(":3000", router)
 }
--- src/go_service/views/layouts/base_templ.go
+++ src/go_service/views/layouts/base_templ.go
@@ -23,7 +23,7 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><body><main>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/bin/styles.css\"></head><body><main>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
