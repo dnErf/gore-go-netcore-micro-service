cbffbc8b47babd7ecf460a16c60f0e2ae5c59bc4
ff <frend.fernando@LIVE.COM>
2024-05-18 21:31:16 -0500

    up test service handler | air & html layout

--- src/go_service/main.go
+++ src/go_service/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gore/views"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,9 +13,70 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", views.RenderHome).Methods("GET")
+	router.HandleFunc("/test-service", HandleTestService).Methods("POST")
 
 	router.PathPrefix("/bin/").Handler(http.StripPrefix("/bin/", http.FileServer(http.Dir("bin"))))
 
 	fmt.Print("listening to :3000\n")
 	http.ListenAndServe(":3000", router)
+}
+
+type DataObject struct {
+	Message string `json:"message"`
+}
+
+type RequestPayload struct {
+	Action string     `json:"action"`
+	Data   DataObject `json:"data"`
+}
+
+type ResponsePaystub struct {
+	IsFine  bool   `json:"is_fine"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func HandleTestService(w http.ResponseWriter, r *http.Request) {
+	var payload RequestPayload
+	err := ReadJSON(w, r, &payload)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%s", payload)
+
+	var paystub ResponsePaystub
+	paystub.IsFine = true
+	paystub.Message = "test is successful"
+	WriteJSON(w, paystub)
+}
+
+const jsonMaxBytes int = 1024 * 10
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	r.Body = http.MaxBytesReader(w, r.Body, int64(jsonMaxBytes))
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return err
+	}
+	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, data any) error {
+	output, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(output)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- src/go_service/views/pages/Home_templ.go
+++ src/go_service/views/pages/Home_templ.go
@@ -31,7 +31,7 @@ func Home() templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"\"><div><h1>micro service</h1><hr><button>Test Service</button><div><span>output shows here...</span></div></div><br><div class=\"\"><div><h2>data sent</h2></div><div><h2>data received</h2></div></div></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"p-4\"><div><h1 class=\"\">micro service</h1><hr><div class=\"my-2 flex flex-row gap-4\"><button id=\"testService\" class=\"border p-2\">Test Service</button> <button class=\"border p-2\">Test Log</button></div></div><div class=\"flex w-full\"><div class=\"w-1/2\"><h2>data sent</h2><pre id=\"sent\" class=\"border px-4\"></pre></div><div class=\"w-0 h-4\"></div><div class=\"w-1/2\"><h2>data received</h2><pre id=\"received\" class=\"border px-4\"></pre></div></div></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -41,6 +41,10 @@ func Home() templ.Component {
 			return templ_7745c5c3_Err
 		})
 		templ_7745c5c3_Err = layouts.Base().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script>\r\n        let sent = document.getElementById(\"sent\")\r\n        let received = document.getElementById(\"received\")\r\n\r\n        let payloadTest = {\r\n            action: \"test\",\r\n            data: {\r\n                message: \"some kind of test\"\r\n            }\r\n        }\r\n        let payStubTest = {\r\n            action: \"test\",\r\n            data: {\r\n                message: \"some kind of test\"\r\n            }\r\n        }\r\n\r\n        sent.innerHTML = \"...\"\r\n        received.innerHTML = \"...\"\r\n\r\n        let testService = document.getElementById(\"testService\")\r\n        testService.addEventListener(\"click\", function() {\r\n            console.log(\"clicked test service\")\r\n            sent.innerHTML = JSON.stringify(payloadTest, undefined, 4)\r\n\r\n            fetch(\"/test-service\", {\r\n                method: \"POST\",\r\n                body: JSON.stringify(payloadTest)\r\n            })\r\n            .then((response) => response.json())\r\n            .then((data) => {\r\n                received.innerHTML = JSON.stringify(data, undefined, 4)\r\n            })\r\n            \r\n            console.log(\"===\")\r\n        })\r\n    </script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
